c1924f95eed94aabcca2e047cb0505f3f907b432
cjvilla <CelineVillareal2020@northwestern.edu>
2021-01-13 15:53:02 -0600

    added total months and total net worth to main.py file

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,15 +3,12 @@ import csv
 
 # Change directory to the directory of current python script
 current_path = os.path.dirname(os.path.abspath(__file__))
-# path to .csv file
 file = os.path.join('Resources', 'budget_data.csv')
 
-#empty lists
-
-net_cost = []
-count = 0
-sum = 0
-
+#initialize variables
+count = 0 # counts number of months
+sum = 0 # counts sum of profits 
+profits = []
 
 # Open and read csv
 with open(file, newline="") as csv_file:
@@ -19,10 +16,11 @@ with open(file, newline="") as csv_file:
     # Read the header row first
     csv_header = next(csv_file)
     for row in csv_reader:
-     count += 1
-     sum += float(row[1])
-    
+        count += 1
+        sum += float(row[1])
+        profits.append(row[1])
+   
 pattern = '-'
 print(f"Financial Analysis \n{pattern * 30} ")
-print(f"Total Months: {count}")
-print(f"Total: {'${:.1f}'.format(sum)}")
+print(f"Total Months: {count} \nTotal: {'${:.1f}'.format(sum)}")
+
