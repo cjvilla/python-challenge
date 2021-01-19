c34e152d827b573aff25176d505d6cfdf4d1dc70
cjvilla <CelineVillareal2020@northwestern.edu>
2021-01-19 17:40:47 -0600

    added counts and percentages of candidates

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,49 +6,48 @@ import sys
 current_path = os.path.dirname(os.path.abspath(__file__))
 file = os.path.join('Resources', 'budget_data.csv')
 
-#initialize variables
-count = 0 # counts number of months
-sum = 0 # sum of profits 
-sum_change = 0 #total change
-profit_changes = [] # holds profit changes 
-max_increase = [0, count] 
-max_decrease = [0, count]
+def find_finances (budget_data):
+    #initialize variables
+    count = 0 # counts number of months
+    sum = 0 # sum of profits 
+    sum_change = 0 #total change
+    profit_changes = [] # holds profit changes 
+    max_increase = [0, count] 
+    max_decrease = [0, count]
 
-# Open and read csv
-with open(file, newline="") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
-    # Read the header row first
-    csv_header = next(csv_file) #header/title info
-    first_row = next(csv_reader) # will iterate to next item
-    prev_row = int(first_row[1])
+    # Open and read csv
+    with open(file, newline="") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        # Read the header row first
+        csv_header = next(csv_file) #header/title info
+        first_row = next(csv_reader) # will iterate to next item
+        prev_row = int(first_row[1])
 
-    for row in csv_reader:
-        count += 1 # number of months
-        sum += float(row[1]) # total profits
-        curr_row = int(row[1])
-        profit_change = int(curr_row - prev_row)
-        profit_changes.append(profit_change)
-        prev_row = int(row[1])
-        sum_change += profit_change
+        for row in csv_reader:
+            count += 1 # number of months
+            sum += float(row[1]) # total profits
+            curr_row = int(row[1])
+            profit_change = int(curr_row - prev_row)
+            profit_changes.append(profit_change)
+            prev_row = int(row[1])
+            sum_change += profit_change
 
-        if profit_change > max_increase[1]:
-           max_increase[0] = str(row[0]) # month of greatest increase
-           max_increase[1] = profit_change # greatest profit
-        if profit_change < max_decrease[1]:
-            max_decrease[0] = str(row[0]) # month of greatest decrease
-            max_decrease[1] = profit_change # greatest decrease 
+            if profit_change > max_increase[1]:
+                max_increase[0] = str(row[0]) # month of greatest increase
+                max_increase[1] = profit_change # greatest profit
+            if profit_change < max_decrease[1]:
+                max_decrease[0] = str(row[0]) # month of greatest decrease
+                max_decrease[1] = profit_change # greatest decrease 
 
-        average_change = sum_change/count 
+    average_change = sum_change/count 
+    print(f"Financial Analysis \n{'-' * 30}")
+    print(f"Total Months: {count} \nTotal: {'${:.1f}'.format(sum)}")
+    print(f"Average Change: {'${:.2f}'.format(average_change)}")
+    print(f"Greatest Increase in Profits: {max_increase[0]} ({'${:.2f}'.format(max_increase[1])}) \nGreatest Decrease in Profits: {max_decrease[0]} ({'${:.2f}'.format(max_decrease[1])}) ")
 
-print(f"Financial Analysis \n{'-' * 30}")
-print(f"Total Months: {count} \nTotal: {'${:.1f}'.format(sum)}")
-print(f"Average Change: {'${:.2f}'.format(average_change)}")
-print(f"Greatest Increase in Profits: {max_increase[0]} ({'${:.2f}'.format(max_increase[1])}) \nGreatest Decrease in Profits: {max_decrease[0]} ({'${:.2f}'.format(max_decrease[1])}) ")
+find_finances(file)
 
 output_file = os.path.join('Analysis', 'financial_analysis.txt')
 sys.stdout = open(output_file, "w")
-print(f"Financial Analysis \n{'-' * 30}")
-print(f"Total Months: {count} \nTotal: {'${:.1f}'.format(sum)}")
-print(f"Average Change: {'${:.2f}'.format(average_change)}")
-print(f"Greatest Increase in Profits: {max_increase[0]} ({'${:.2f}'.format(max_increase[1])}) \nGreatest Decrease in Profits: {max_decrease[0]} ({'${:.2f}'.format(max_decrease[1])}) ")
+print(find_finances(file))
 sys.stdout.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,27 @@
 import os
 import csv
 import sys
-
+import operator 
 current_path = os.path.dirname(os.path.abspath(__file__))
 file = os.path.join('Resources', 'election_data.csv')
 
+def convert_to_percentage(count, sum):
+    total = (count/sum) * 100
+    return total
+
 def print_election_results(election_data):
+    #variables
     count = 0
+    khan_count = 0
+    khan_percent = 0
+    correy_count = 0
+    correy_percent = 0
+    li_count = 0
+    li_percent = 0
+    otoole_count = 0
+    otoole_percent = 0
     hold_candidates = [] # list of candidates
+    
 
     with open(file, newline="") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
@@ -15,11 +29,34 @@ def print_election_results(election_data):
         for row in csv_reader:
             count += 1
             hold_candidates.append(row[2])
-   
-    print(f"Election Results \n{'-' * 30} \nTotal Votes: {count}")
-    print(f"{len(set(hold_candidates)) - 1}") #starts at 0
+        for i in hold_candidates:
+            if i == "Khan":
+                khan_count += 1
+                khan_percent = convert_to_percentage(khan_count, count)
+            if i == "Correy":
+                correy_count += 1
+                correy_percent = convert_to_percentage(correy_count, count)
+            if i == "Li":
+                li_count += 1
+                li_percent = convert_to_percentage(li_count, count)
+            if i == "O'Tooley":
+                otoole_count += 1
+                otoole_percent = convert_to_percentage(otoole_count, count)
+        
+        candidate_dict = {
+            "Khan": khan_count,
+            "Correy": correy_count,
+            "Li": li_count,
+            "O'Tooley": otoole_count
+        }
+
+    print(f"Election Results \n{'-' * 30} \nTotal Votes: {count} \n{'-' * 30}")
+    print(f"Khan : {'{:.3f}'.format(khan_percent)}% ({khan_count})")
+    print(f"Correy : {'{:.3f}'.format(correy_percent)}% ({correy_count})")
+    print(f"Li : {'{:.3f}'.format(li_percent)}% ({li_count})")
+    print(f"O'Tooley : {'{:.3f}'.format(otoole_percent)}% ({otoole_count}) \n{'-' * 30}")
+    print(f"Winner: {max}")
     
-    
-    
+    # print(set(hold_candidates)) prints unique set of candidates
 print_election_results(file)
    
